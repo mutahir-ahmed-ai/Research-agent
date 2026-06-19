7b74a77ab34e3c0820c0c13088b3f29ac50d3bfd
Mutahir-Ahmed <mutahirahmedc745@gmail.com>
2026-06-19 21:34:59 +0500

    research_agent.py

---
+++ agent/research_agent.py
@@ -0,0 +1,30 @@
+import os
+from langchain_groq import ChatGroq
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain import hub
+from tools.search_tool import search_web
+from tools.pdf_tool import generate_pdf_report
+
+def create_research_agent():
+    
+    llm = ChatGroq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+        model_name="llama-3.3-70b-versatile",
+        temperature=0.3
+    )
+
+    tools = [search_web, generate_pdf_report]
+
+    prompt = hub.pull("hwchase17/react")
+
+    agent = create_react_agent(llm, tools, prompt)
+
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        max_iterations=10,
+        handle_parsing_errors=True
+    )
+
+    return agent_executor
