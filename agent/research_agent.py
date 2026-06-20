9f0a445fdfdb7aada6256e40588d741fd1f7de40
Mutahir-Ahmed <mutahirahmedc745@gmail.com>
2026-06-20 19:55:57 +0500

    research_agent.py

--- agent/research_agent.py
+++ agent/research_agent.py
@@ -1,7 +1,7 @@
 import os
 from langchain_groq import ChatGroq
 from langchain_classic.agents import AgentExecutor, create_react_agent
-from langchain_classic import hub
+from langchain_core.prompts import PromptTemplate
 from tools.search_tool import search_web
 from tools.pdf_tool import generate_pdf_report
 
@@ -15,7 +15,27 @@ def create_research_agent():
 
     tools = [search_web, generate_pdf_report]
 
-    prompt = hub.pull("hwchase17/react")
+    template = """Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}"""
+
+    prompt = PromptTemplate.from_template(template)
 
     agent = create_react_agent(llm, tools, prompt)
 
